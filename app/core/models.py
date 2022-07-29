f81eadc5a609463847d25f124071e3e9c77b66cc
Paulo Alexandre Silva <pacalexandre@gmail.com>
2022-07-29 22:25:09 +0000

    atualizando

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,6 @@
 from django.db import models
 
 
-
 class Categoria(models.Model):
     nome = models.CharField(max_length=100, blank=True, null=True)
 
@@ -12,17 +11,19 @@ class Categoria(models.Model):
     class Meta:
         db_table = "tb_categoria"
 
+
 class Produto(models.Model):
-    codigo = models.IntegerField(default=0 , blank=True, null=True)
+    codigo = models.IntegerField(default=0, blank=True, null=True)
     nome = models.CharField(max_length=200, blank=True, null=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    preco = models.DecimalField(default=0,decimal_places=2, max_digits=10)
+    preco = models.DecimalField(default=0, decimal_places=2, max_digits=10)
 
     def __str__(self):
         return str(self.nome)
 
     class Meta:
         db_table = "tb_produto"
+
 
 class NotaFiscal(models.Model):
     numero = models.IntegerField(unique=True, auto_created=True)
